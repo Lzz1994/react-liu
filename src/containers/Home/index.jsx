bd06d7eea61aa59362a7e8192da8e18d20b50877
xiadd <xiadd0102@gmail.com>
2018-04-03 14:39:43 +0800

    add config

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import './index.less'
 
+import Loader from 'components/Loader'
+
 class Home extends Component {
   render () {
-    return <h1>Home</h1>
+    return <Loader color="#aaa" type="line-scale" style={{ textAlign: 'center', marginTop: '13rem' }} />
   }
 }
 
