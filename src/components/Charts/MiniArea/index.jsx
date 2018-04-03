1b887d7979e5c9565e46522bbc67d4f073ff2174
xiadd <xiadd0102@gmail.com>
2018-04-03 20:38:26 +0800

    create area chart

---
+++ src/components/Charts/MiniArea/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class MiniAreaChart extends Component {
+
+  componentDidMount () {
+    
+  }
+
+  render () {
+    return <h1>Chart</h1>
+  }
+}
+
+export default MiniAreaChart
--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 import './index.less'
 
-import Loader from 'components/Loader'
+import MiniAreaChart from 'components/Charts/MiniArea'
 
 class Home extends Component {
   render () {
-    return <Loader color="#aaa" type="line-scale" style={{ textAlign: 'center' }} />
+    return <MiniAreaChart />
   }
 }
 
